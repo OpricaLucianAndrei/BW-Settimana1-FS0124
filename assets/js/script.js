7f9d92b8df77962dde3120af8a4aa9a84f04d964
Gianluca <gianluca.giacchetta@gmail.com>
2024-02-12 16:35:02 +0000

    update javascrtipt

---
+++ assets/js/script.js
@@ -0,0 +1,23 @@
+const form=document.getElementsByTagName('form');
+const btnAdd=document.getElementById('btnAdd');
+const checkbox=document.getElementById('checkbox');
+
+window.addEventListener("load", init());
+
+function init() {
+  btnStart();
+}
+function btnStart() {
+  document.getElementById("btnAdd").disabled = true;
+  checkbox.addEventListener('click',function () {
+  if (!checkbox.checked) {
+      document.getElementById("btnAdd").disabled = true;
+  }else
+  {
+      document.getElementById("btnAdd").disabled =false;
+  }
+
+})
+}
+
+
