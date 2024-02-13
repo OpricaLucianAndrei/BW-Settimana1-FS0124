0b933f96d8fa4ec7f51947c9adc056687cb08f49
Gianluca <gianluca.giacchetta@gmail.com>
2024-02-13 23:09:34 +0100

    Update script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,27 +1,3 @@
-
-const form = document.getElementsByTagName('form');
-const btnAdd = document.getElementById('btnAdd');
-const checkbox = document.getElementById('checkbox');
-
-window.addEventListener("load", init());
-
-function init() {
-  btnStart();
-}
-function btnStart() {
-  btnAdd.disabled = true;
-  checkbox.addEventListener('click', function () {
-    if (checkbox.checked) {
-      btnAdd.disabled = false;
-      btnAdd.classList.add('btnEnabled');
-    } else {
-      btnAdd.disabled = true;
-      btnAdd.classList.remove('btnEnabled');
-    }
-
-  })
-}
-
 const form = document.getElementsByTagName('form');
 const btnAdd = document.getElementById('btnAdd');
 const checkbox = document.getElementById('checkbox');
